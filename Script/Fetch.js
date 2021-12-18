0fe1462e7fa87340b06dd890cdf98575b3ada6d5
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-12-19 04:40:05 +0700

    Fix Maximum call stack size exceeded

--- Script/Fetch.js
+++ Script/Fetch.js
@@ -1,9 +1,8 @@
-
-function fetch() {
-    setTimeout(function () {
-        fetch('https://top.gg/api/bots/706144670807228568').then(response => {
-            let data = response.json();
+function fetch2() {
+    fetch('https://top.gg/api//bots/706144670807228568').then(response => {
+        response.json().then(data => {
+            console.log(data);
             return document.querySelector("h3").innerHTML = `Ayaka 1 - Server Count: ${data.server_count}`;
         });
-    }, 1000);
+    })
 }
