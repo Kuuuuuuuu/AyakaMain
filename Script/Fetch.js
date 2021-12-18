beeb8a830adcf61c35413c3008b54e55e32a3d74
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-12-19 04:24:07 +0700

    sdsdsds

--- Script/Fetch.js
+++ Script/Fetch.js
@@ -1,7 +1,9 @@
 
 function fetch() {
-    fetch('https://top.gg/api//bots/706144670807228568').then(response => {
-        let data = response.json();
-        return document.querySelector("h3").innerHTML = `Ayaka 1 - Server Count: ${data.server_count}`;
-    });
+    setTimeout(function () {
+        fetch('https://top.gg/api/bots/706144670807228568').then(response => {
+            let data = response.json();
+            return document.querySelector("h3").innerHTML = `Ayaka 1 - Server Count: ${data.server_count}`;
+        });
+    }, 1000);
 }
